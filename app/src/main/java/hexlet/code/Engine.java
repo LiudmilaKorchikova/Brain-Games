07dff8770e939a39fadbec6336788be1d7d09696
Liudmila Korchikova <liudmila.korchikova@outlook.com>
2024-08-10 17:40:04 +0200

    Magic numbers corrected 3

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 import static hexlet.code.Cli.getUserName;
 
 public class Engine {
+    private static final int QUESTIONS_COUNT = 3;
     public static void playGame(String[] expected, String[] question, String rules) {
         String userName = getUserName();
         System.out.println(rules);
         var scanner = new Scanner(System.in);
-        int questionsCount = 3;
+        int questionsCount = QUESTIONS_COUNT;
         for (int i = 0; i < questionsCount; i++) {
             System.out.println("Question: " + question[i]);
             System.out.print("Your answer: ");
